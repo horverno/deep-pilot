683bfd92039b6ebbcfcfe6c84a40ad29fe60ca91
Ernő Horváth <horverno@gmail.com>
2017-07-06 08:42:22 +0200

    Initial commit of the C# branch

---
+++ DeepPilotCsharp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeepPilotCsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool MenuSmall = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TabHome.Visibility = Visibility.Collapsed;
+            TabSettings.Visibility = Visibility.Collapsed;
+            TabRaw.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuSmall)
+            {
+                MenuColumn.Width = new GridLength(100, GridUnitType.Auto);
+                MenuSmall = false;
+            }
+            else
+            {
+                MenuColumn.Width = new GridLength(44, GridUnitType.Pixel);
+                MenuSmall = true;
+            }
+        }
+
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
+            TabMain.SelectedIndex = 0;
+            BtnHome.Foreground = Brushes.DarkMagenta;
+            BtnSett.Foreground = Brushes.Gray;
+            BtnRaw.Foreground = Brushes.Gray;
+        }
+
+        private void BtnSett_Click(object sender, RoutedEventArgs e)
+        {
+            TabMain.SelectedIndex = 1;
+            BtnHome.Foreground = Brushes.Gray;
+            BtnSett.Foreground = Brushes.DarkMagenta;
+            BtnRaw.Foreground = Brushes.Gray;
+        }
+
+        private void BtnRaw_Click(object sender, RoutedEventArgs e)
+        {
+            TabMain.SelectedIndex = 2;
+            BtnHome.Foreground = Brushes.Gray;
+            BtnSett.Foreground = Brushes.Gray;
+            BtnRaw.Foreground = Brushes.DarkMagenta;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Deep pilot
 A Raspberry Pi based model vehicle driven by deep convolutional neural network.
 
-## Branches
-The branches represent where the code runs: there is an `rpi` branch for the vehicle and there are `pc`-annotated branches for different language realizations, e.g. `pc-csharp` for the c# based application. 
-
-`git clone -b rpi https://github.com/horverno/deep-pilot`
-
-`git clone -b pc-csharp https://github.com/horverno/deep-pilot`
+## C# branch
+This branch contains codes from the PC side realized in C# and XAML.
