03ad5522d07a37c3710968f04357d336afd7188e
Ernő Horváth <horverno@gmail.com>
2017-07-19 15:11:19 +0000

    Motor functionality extended

--- rpiUdpJson.py
+++ rpiUdpJson.py
@@ -14,9 +14,8 @@ import socket
 import json
 import threading
 import time
-import pygame, sys
-import pygame.camera
-from pygame.locals import *
+import picamera
+import picamera.array
 from ax12 import Ax12
 
 udpIpRead = "192.168.0.1"
@@ -53,6 +52,9 @@ def sendMsg():
         #print(json.dumps(sendDict))
         if "MotorPositionDrive" in sendDict:
             sendDict["MotorPositionDrive"] = axMot.readPosition(idMotDrive)
+        if "VehicleSpeedDyna" in sendDict:
+            sendDict["VehicleSpeedDyna"] = axMot.readSpeed(idMotDrive)
+            print(axMot.readSpeed(idMotDrive))
         sockS.sendto((json.dumps(sendDict)).encode('utf-8'), (udpIpSend, udpPortSend))
     #print(" >> Write message thread stopped.")
 
@@ -60,81 +62,89 @@ def readMsg():
     global exitFlag, udpIpSend, sendDelay, sendDict, axMot, idMotSteering, idMotDrive
     #print("Read message thread started.")
     while not exitFlag:
-        #print("r %d" %i)
-        #time.sleep(2)
         readData, addr = sockR.recvfrom(1024) # buffer size is 1024 bytes
-        print("R: %s" % readData)
         try:
             readDict = json.loads(str(readData,'utf-8'))
             for d in readDict:
-                    if("Exit" == d):
-                            exitFlag = True
-                    if("SetUdpIpSend" == d):
-                            udpIpSend = readDict["SetUdpIpSend"]
-                            print("New ip: %s" % (udpIpSend))
-                    if("SetDelay" == d):
-                            sendDelay = float(readDict["SetDelay"])
-                            print(" >> New frequency: %.2f" % (1 / sendDelay))
-                    if("SetSpeedSignalReferenceDrive" == d):
-                            axMot.setMovingSpeed(idMotDrive, readDict["SetSpeedSignalReferenceDrive"])
-                            print(" >> New wehicle speed: %.1f" % (readDict["SetSpeedSignalReferenceDrive"]))
-                    if("SetAngleReferenceSteering" == d):
-                            axMot.move(idMotSteering, readDict["SetAngleReferenceSteering"])
-                            print(" >> New steering angle: %.1f" % (readDict["SetAngleReferenceSteering"]))            
-                    if("SendMotorPosition" == d):
-                            if readDict["SendMotorPosition"] == True:
-                                    sendDict["MotorPositionDrive"] = 0
-                                    print(" >> Sending motor position too...")
-                            else:
-                                    try:
-                                            del sendDict["MotorPositionDrive"]
-                                            break
-                                    except:
-                                            print(" >> Error deleting MotorPositionDrive")
-                                    print(" >> Stopped sending motor position...")            
-                    if("SendLapNumber" == d):
-                            if readDict["SendLapNumber"] == True:
-                                    sendDict["LapNumber"] = 0
-                                    print(" >> Sending LapNumber too...")
-                            else:
-                                    try:
-                                            del sendDict["LapNumber"]
-                                            break
-                                    except:
-                                            print(" >> Error deleting LapNumber")
-                                    print(" >> Stopped sending LapNumber...")
-                    if("SendCamera1" == d):
-                            if readDict["SendCamera1"] == True:
-                                    #snap1 = snapShotImage()
-                                    #sendDict["Camera1"] = str(pygame.image.tostring(snap1, "RGBA", True))
-                                    #sendDict["Camera1"] = "ff"
-                                    #print(pygame.image.tostring(snap1, "RGBA", True))
-                                    print(" >> Sending cam image[1] too via TCP...")
-                            else:
-                                    try:
-                                            del sendDict["Camera1"]
-                                            break
-                                    except:
-                                            print(" >> Error deleting Camera1 entry")
-                                    print(" >> Stopped sending cam image[1]...")        
-                    break
+                if("Exit" == d):
+                    exitFlag = True
+                elif("SetUdpIpSend" == d):
+                    udpIpSend = readDict["SetUdpIpSend"]
+                    print("New ip: %s" % (udpIpSend))
+                elif("SetDelay" == d):
+                    sendDelay = float(readDict["SetDelay"])
+                    print(" >> New frequency: %.2f" % (1 / sendDelay))
+                elif("SetSpeedSignalReferenceDrive" == d):
+                    #time.sleep(0.2)
+                    axMot.setMovingSpeed(idMotDrive, readDict["SetSpeedSignalReferenceDrive"])
+                    print(" >> New vehicle speed: %.1f" % (readDict["SetSpeedSignalReferenceDrive"]))
+                elif("SetAngleReferenceSteering" == d):
+                    axMot.move(idMotSteering, readDict["SetAngleReferenceSteering"])
+                    print(" >> New steering angle: %.1f" % (readDict["SetAngleReferenceSteering"]))            
+                elif("SendMotorPositionDrive" == d):
+                    if readDict["SendMotorPositionDrive"] == True:
+                        sendDict["MotorPositionDrive"] = 0
+                        print(" >> Sending motor position too...")
+                    else:
+                        try:
+                            del sendDict["MotorPositionDrive"]
+                            break
+                        except:
+                            print(" >> Error deleting MotorPositionDrive")
+                        print(" >> Stopped sending motor position...")
+                elif("SendMotorSpeedDrive" == d):
+                    if readDict["SendMotorSpeedDrive"] == True:
+                        sendDict["VehicleSpeedDyna"] = 0
+                        print(" >> Sending motor speed (VehicleSpeedDyna) too...")
+                    else:
+                        try:
+                            del sendDict["VehicleSpeedDyna"]
+                            break
+                        except:
+                            print(" >> Error deleting VehicleSpeedDyna")
+                        print(" >> Stopped sending motor position...")         
+                elif("SendLapNumber" == d):
+                    if readDict["SendLapNumber"] == True:
+                        sendDict["LapNumber"] = 0
+                        print(" >> Sending LapNumber too...")
+                    else:
+                        try:
+                            del sendDict["LapNumber"]
+                            break
+                        except:
+                            print(" >> Error deleting LapNumber")
+                        print(" >> Stopped sending LapNumber...")
+                elif("SendCamera1" == d):
+                    if readDict["SendCamera1"] == True:
+                        snap1 = snapShotImage()
+                        print(" >> Sending cam image[1] too via TCP...")
+                    else:
+                        try:
+                            del sendDict["Camera1"]
+                            break
+                        except:
+                            print(" >> Error deleting Camera1 entry")
+                        print(" >> Stopped sending cam image[1]...")
+                else:
+                    print("R: %s not recognized" % str(readData,'utf-8'))
+                break
         except:
             print(" >> Not valid JSON sting recieved.")
-        #print(" >> Read message thread stopped.")
+    print(" >> Read message thread stopped.")
 
 
 def snapShotImage():
     global width1, height1
-    cam1 = pygame.camera.Camera("/dev/video2",(width1,height1))
-    cam1.start()
-    image = cam1.get_image()
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiYUVArray(camera) as output:
+            camera.capture(output, 'yuv')
+            #print('Captured %dx%d image' % (output.array.shape[1], output.array.shape[0]))
+            image = output
     return image
 
 # main
 
 print("Listening... (waiting for exit message)")
-pygame.init() # camera
-pygame.camera.init() # camera
 # Motor endless turn - If both values for the CW Angle Limit and the CCW Angle Limit are set to 0, an Endless Turn mode can be implemented by setting the Moving Speed
 axMot.setAngleLimit(idMotDrive, 0, 0)
 axMot.setAngleLimit(idMotSteering, 0, 1023)
